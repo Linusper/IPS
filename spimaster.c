5d43ef16e703b8b31474771cb3814ef8c171d651
Linusper <55286577+Linusper@users.noreply.github.com>
2020-03-05 13:53:16 +0100

    Add files via upload

--- spimaster.c
+++ spimaster.c
@@ -52,44 +52,87 @@
 
 #define THREADSTACKSIZE (1024)
 
-#define SPI_MSG_LENGTH  (2)
-
-#define MAX_LOOP        (10)
-
-
-// ---------- ADDRESSES -----------
-#define POWER_CTL       (0x2d)
-#define DATA_FORMAT     (0x31)
-#define FIFO_CTL        (0x38)
-
-#define DATAX0          (0x32)
-#define DATAX1          (0x33)
-
-#define DATAY0          (0x34)
-#define DATAY1          (0x35)
-
-#define DATAZ0          (0x36)
-#define DATAZ1          (0x37)
-// --------------------------------
-
-static Display_Handle display;
+#define SPI_MSG_LENGTH  (20)
 
 unsigned char masterRxBuffer[SPI_MSG_LENGTH];
 unsigned char masterTxBuffer[SPI_MSG_LENGTH];
 
-bool breakLoop = false;
+// ---------- REGISTERS ---------------
+
+#define DEVID           (0x00) /**< Device ID */
+#define THRESH_TAP      (0x1D) /**< Tap threshold */
+#define OFSX            (0x1E) /**< X-axis offset */
+#define OFSY            (0x1F) /**< Y-axis offset */
+#define OFSZ            (0x20) /**< Z-axis offset */
+#define DUR             (0x21) /**< Tap duration */
+#define LATENT          (0x22) /**< Tap latency */
+#define WINDOW          (0x23) /**< Tap window */
+#define THRESH_ACT      (0x24) /**< Activity threshold */
+#define THRESH_INACT    (0x25) /**< Inactivity threshold */
+#define TIME_INACT      (0x26) /**< Inactivity time */
+#define ACT_INACT_CTL   (0x27) /**< Axis enable control for activity and inactivity detection */
+#define THRESH_FF       (0x28) /**< Free-fall threshold */
+#define TIME_FF         (0x29) /**< Free-fall time */
+#define TAP_AXES        (0x2A) /**< Axis control for single/double tap */
+#define ACT_TAP_STATUS  (0x2B) /**< Source for single/double tap */
+#define BW_RATE         (0x2C) /**< Data rate and power mode control */
+#define POWER_CTL       (0x2D) /**< Power-saving features control */
+#define INT_ENABLE      (0x2E) /**< Interrupt enable control */
+#define INT_MAP         (0x2F) /**< Interrupt mapping control */
+#define INT_SOURCE      (0x30) /**< Source of interrupts */
+#define DATA_FORMAT     (0x31) /**< Data format control */
+#define DATAX0          (0x32) /**< X-axis data 0 */
+#define DATAX1          (0x33) /**< X-axis data 1 */
+#define DATAY0          (0x34) /**< Y-axis data 0 */
+#define DATAY1          (0x35) /**< Y-axis data 1 */
+#define DATAZ0          (0x36) /**< Z-axis data 0 */
+#define DATAZ1          (0x37) /**< Z-axis data 1 */
+#define FIFO_CTL        (0x38) /**< FIFO control */
+#define FIFO_STATUS     (0x39) /**< FIFO status */
+
+// ------------------------------------
+
+// ---------- REGISTERS  SETTINGS ---------------
+
+// ----------------------------------------------
+
+static Display_Handle display;
+
+bool breakProgramLoop = false;
+bool breakMeasureLoop = false;
+
+typedef struct {
+    int16_t x;
+    int16_t y;
+    int16_t z;
+} RawData_t;
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+} AccData_t;
 
 void gpioButtonFxn0(uint_least8_t index)
 {
-    breakLoop = true;
+    if (breakMeasureLoop){
+        breakMeasureLoop = false;
+    }else{
+        breakMeasureLoop = true;
+    }
 }
 
-void readAddress(SPI_Handle handle, SPI_Transaction *transaction, uint8_t address, uint8_t *p_data, uint8_t bytes){
+void gpioButtonFxn1(uint_least8_t index)
+{
+    breakProgramLoop = true;
+}
 
+void readAddress(SPI_Handle handle, uint8_t address, uint8_t *p_data, uint8_t bytes){
 
     bool transferOK;
-    uint8_t masterTxBuffer[7];
-    memset((void *) masterTxBuffer, 0, 7);
+    SPI_Transaction transaction;
+    memset((void *) masterTxBuffer, 0, SPI_MSG_LENGTH);
+    memset((void *) masterRxBuffer, 0, SPI_MSG_LENGTH);
 
     if(bytes > 1){
         masterTxBuffer[0] = (address | 0x80) | 0x40;
@@ -98,13 +141,16 @@ void readAddress(SPI_Handle handle, SPI_Transaction *transaction, uint8_t addres
         masterTxBuffer[0] = address | 0x80;
     }
 
-    transaction->count = bytes+1;
-    transaction->txBuf = (void *) masterTxBuffer;
-    transaction->rxBuf = (void *) p_data;
+    transaction.count = bytes+1;
+    transaction.txBuf = (void *) masterTxBuffer;
+    transaction.rxBuf = (void *) masterRxBuffer;
 
-    transferOK = SPI_transfer(handle, transaction);
+    transferOK = SPI_transfer(handle, &transaction);
     if (transferOK) {
-        //Display_printf(display, 0, 0, "Rx Buffer 1: %x   2: %x   3: %x   4: %x   5: %x   6: %x", p_data[0], p_data[1], p_data[2], p_data[3], p_data[4], p_data[5]);
+        uint8_t i;
+        for (i = 0; i < bytes; i++){
+            p_data[i] = masterRxBuffer[i+1];
+        }
     }
     else {
         Display_printf(display, 0, 0, "Unsuccessful master SPI transfer");
@@ -112,20 +158,19 @@ void readAddress(SPI_Handle handle, SPI_Transaction *transaction, uint8_t addres
 
 }
 
-void writeAddress(SPI_Handle handle, SPI_Transaction *transaction, uint8_t address, uint8_t data){
+void writeAddress(SPI_Handle handle, uint8_t address, uint8_t data){
 
     bool transferOK;
-    uint8_t masterTxBuffer[2];
+    SPI_Transaction transaction;
+
     masterTxBuffer[0] = address & 0x3f;
     masterTxBuffer[1] = data;
-    uint8_t masterRxBuffer[2];
-    memset((void *) masterRxBuffer, 0, 2);
 
-    transaction->count = 2;
-    transaction->txBuf = (void *) masterTxBuffer;
-    transaction->rxBuf = (void *) masterRxBuffer;
+    transaction.count = 2;
+    transaction.txBuf = (void *) masterTxBuffer;
+    transaction.rxBuf = (void *) masterRxBuffer;
 
-    transferOK = SPI_transfer(handle, transaction);
+    transferOK = SPI_transfer(handle, &transaction);
     if (transferOK) {
         Display_printf(display, 0, 0, "Master sent: %x to address %x", masterTxBuffer[1], address);
     }
@@ -134,23 +179,59 @@ void writeAddress(SPI_Handle handle, SPI_Transaction *transaction, uint8_t addre
     }
 }
 
-void readRawData(SPI_Handle handle, SPI_Transaction *transaction){
+void readRawData(SPI_Handle handle, RawData_t *data){
 
-    uint8_t rxBuffer[7];
+    uint8_t rxBuffer[6];
     int16_t rawX = 0x0000;
     int16_t rawY = 0x0000;
     int16_t rawZ = 0x0000;
 
-    memset((void *) rxBuffer, 0, 7);
+    memset((void *) rxBuffer, 0, 6);
 
-    readAddress(handle, transaction, DATAX0, rxBuffer, 7);
+    readAddress(handle, DATAX0, rxBuffer, 6);
 
-    rawX = (rxBuffer[2]<<8) | rxBuffer[1];
-    rawY = (rxBuffer[4]<<8) | rxBuffer[3];
-    rawZ = (rxBuffer[6]<<8) | rxBuffer[5];
+    rawX = (rxBuffer[1]<<8) | rxBuffer[0];
+    rawY = (rxBuffer[3]<<8) | rxBuffer[2];
+    rawZ = (rxBuffer[5]<<8) | rxBuffer[4];
 
-    Display_printf(display, 0, 0, "X: %f, Y: %f, Z: %f", (float) 3.9 * rawX, (float) 3.9 *  rawY, (float) 3.9 *  rawZ);
+    data->x = rawX;
+    data->y = rawY;
+    data->z = rawZ;
 
+}
+
+void readAccData(SPI_Handle handle, AccData_t *data){
+
+    RawData_t rawData;
+    readRawData(handle, &rawData);
+
+    data->x = ((float) rawData.x * 3.9 * 9.81) / 1000;
+    data->y = ((float) rawData.y * 3.9 * 9.81) / 1000;
+    data->z = ((float) rawData.z * 3.9 * 9.81) / 1000;
+
+}
+
+void initAccelerometer(SPI_Handle handle){
+    //------ STANDBY MODE FOR CHANGING SETTINGS ---------
+    writeAddress(handle, POWER_CTL, 0x00);
+
+    //--------- CHANGE DATA FORMAT ----------------
+    /*        D1    D0
+     * -----------------
+     * ±2   |  0  |  0  |
+     * ±4   |  0  |  1  |
+     * ±8   |  1  |  0  |
+     * ±16  |  1  |  1  |
+     */
+    writeAddress(handle, DATA_FORMAT, 0x00);
+
+    //----------- FIFO SETTING -----------
+    // 0x00 - default
+    // 0x9f - stream mode, trigger 0, samples 1
+    writeAddress(handle, FIFO_CTL, 0x9f);
+
+    //----------- TURN ON MEASUREMENT -----------
+    writeAddress(handle, POWER_CTL, 0x08);
 }
 
 /*
@@ -162,7 +243,8 @@ void *masterThread(void *arg0)
 {
     SPI_Handle      masterSpi;
     SPI_Params      spiParams;
-    SPI_Transaction transaction;
+    AccData_t       accData;
+    RawData_t       rawData;
 
     /* Open SPI as master (default) */
     SPI_Params_init(&spiParams);
@@ -177,34 +259,32 @@ void *masterThread(void *arg0)
     else {
         Display_printf(display, 0, 0, "Master SPI initialized\n");
     }
-    uint8_t deviceID[2];
-    readAddress(masterSpi, &transaction, 0x00, deviceID, (uint8_t) 1);
-    Display_printf(display, 0, 0, "Device ID: %x", deviceID[1]);
 
 
-    //------ STANDBY MODE FOR CHANGING SETTINGS ---------
-    writeAddress(masterSpi, &transaction, POWER_CTL, 0x00);
+    uint8_t deviceID;
+    readAddress(masterSpi, 0x00, &deviceID, (uint8_t) 1);
+    Display_printf(display, 0, 0, "Device ID: %x", deviceID);
 
-    //--------- CHANGE DATA FORMAT ----------------
-    writeAddress(masterSpi, &transaction, DATA_FORMAT, 0x00);
 
-    //----------- FIFO SETTING -----------
-    // 0x00 - default
-    // 0x9f - stream mode, trigger 0, samples 1
-    writeAddress(masterSpi, &transaction, FIFO_CTL, 0x9f);
+    //------ Initialize the accelerometer ---------
+    initAccelerometer(masterSpi);
 
-    //----------- TURN ON MEASUREMENT -----------
-    writeAddress(masterSpi, &transaction, POWER_CTL, 0x08);
+    while(!breakProgramLoop){
 
-    while(!breakLoop){
+        while(!breakMeasureLoop){
 
-        /* Toggle user LED, indicating a SPI transfer is in progress */
-        GPIO_toggle(Board_GPIO_LED1);
+            /* Toggle user LED, indicating a SPI transfer is in progress */
+            GPIO_toggle(Board_GPIO_LED1);
 
-        readRawData(masterSpi, &transaction);
+            readRawData(masterSpi, &rawData);
+            Display_printf(display, 0, 0, "RAW  X: %d, Y: %d, Z: %d", rawData.x, rawData.y, rawData.z);
+            readAccData(masterSpi, &accData);
+            Display_printf(display, 0, 0, "ACC  X: %f, Y: %f, Z: %f", accData.x, accData.y, accData.z);
 
-        /* Sleep for a bit before starting the next SPI transfer  */
-        //sleep(1);
+            /* Sleep for a bit before starting the next SPI transfer  */
+            //sleep(1);
+        }
+
     }
 
     SPI_close(masterSpi);
@@ -237,6 +317,10 @@ void *mainThread(void *arg0)
     GPIO_setConfig(Board_GPIO_BUTTON0, GPIO_CFG_IN_PU | GPIO_CFG_IN_INT_FALLING);
     GPIO_setCallback(Board_GPIO_BUTTON0, gpioButtonFxn0);
     GPIO_enableInt(Board_GPIO_BUTTON0);
+
+    GPIO_setConfig(Board_GPIO_BUTTON1, GPIO_CFG_IN_PU | GPIO_CFG_IN_INT_FALLING);
+    GPIO_setCallback(Board_GPIO_BUTTON1, gpioButtonFxn1);
+    GPIO_enableInt(Board_GPIO_BUTTON1);
 
     /* Open the display for output */
     display = Display_open(Display_Type_UART, NULL);
@@ -279,6 +363,3 @@ void *mainThread(void *arg0)
 
     return (NULL);
 }
-
-
-
